3b32e35ff58fc34e6fd58402269b2bc7809a11e4
Andrey Adamovich <andrey@aestasit.com>
2016-08-20 21:31:33 +0300

    updated for defensive generation

---
+++ render_all.cmd
@@ -0,0 +1,6 @@
+@call gradlew clean
+@call gradlew build -S -PdataFile=E:/Work/AESTAS/WEBSITES/certs.devchampions.com/data_eh_2016.03.10.csv -PcertificateTemplate=certificate_eh_blue_2016.03.10.svg 
+@call gradlew build -S -PdataFile=E:/Work/AESTAS/WEBSITES/certs.devchampions.com/data_sk_2016.03.15.csv -PcertificateTemplate=certificate_sk_blue_2016.03.15.svg 
+@call gradlew build -S -PdataFile=E:/Work/AESTAS/WEBSITES/certs.devchampions.com/data_cd_2016.04.21.csv -PcertificateTemplate=certificate_cd_blue_2016.04.21.svg 
+@call gradlew build -S -PdataFile=E:/Work/AESTAS/WEBSITES/certs.devchampions.com/data_dm_2016.05.09.csv -PcertificateTemplate=certificate_dm_blue_2016.05.09.svg 
+@call gradlew build -S -PdataFile=E:/Work/AESTAS/WEBSITES/certs.devchampions.com/data_d1_2016.08.22.csv -PcertificateTemplate=certificate_d1_blue_2016.08.22.svg 
