5f7311bf9d89ad38e472abfa1f0c11bc7fcae1ef
Andrey Adamovich <andrey@aestasit.com>
2016-08-31 12:00:02 +0300

    added more courses

--- render_all.cmd
+++ render_all.cmd
@@ -5,3 +5,4 @@
 @call gradlew build -S -PdataFile=%CERT_HOME%/data_cd_2016.04.21.csv -PcertificateTemplate=certificate_cd_blue_2016.04.21.svg 
 @call gradlew build -S -PdataFile=%CERT_HOME%/data_dm_2016.05.09.csv -PcertificateTemplate=certificate_dm_blue_2016.05.09.svg 
 @call gradlew build -S -PdataFile=%CERT_HOME%/data_d1_2016.08.22.csv -PcertificateTemplate=certificate_d1_blue_2016.08.22.svg 
+@call gradlew build -S -PdataFile=%CERT_HOME%/data_j8_2016.09.14.csv -PcertificateTemplate=certificate_j8_blue_2016.09.14.svg 
